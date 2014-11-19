a952fb5dc35e3b7da795de1b23412a3da1fd2ec7
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-11-19 19:05:43 +0100

    trying to get number of tweets per day for #isTDDDead

---
+++ istddisdead-twitter/src/main/java/name/lemerdy/istdddead/IsTDDDeadFromTwitter.java
@@ -0,0 +1,55 @@
+package name.lemerdy.istdddead;
+
+import twitter4j.*;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+
+public class IsTDDDeadFromTwitter {
+
+    private final QueryResult result;
+
+    public IsTDDDeadFromTwitter(QueryResult result) {
+        this.result = result;
+    }
+
+    public static void main(String[] args) {
+        Configuration twitterAuthenticationConfiguration = new ConfigurationBuilder()
+                .setOAuthConsumerKey("5eL3ACRPhze3Y5i8LBajaE721")
+                .setOAuthConsumerSecret("GRC1b0aW2x4l1ehqFmaQZhPFCIAu7x5YOkESw1bbQCSrHFFDAe")
+                .setOAuthAccessToken("15370457-qwr2yYCZ7WFf13JbId5LcfNID0WKwvceegrerI1Uc")
+                .setOAuthAccessTokenSecret("1crMxhavzZqk3HUl2jbx1EQiiGhXFY1Lpt7ZPZP28FLLu")
+                .build();
+        Twitter twitter = new TwitterFactory(twitterAuthenticationConfiguration).getInstance();
+
+        QueryResult result;
+        try {
+            result = twitter.search(new Query("#isTDDDead"));
+        } catch (TwitterException e) {
+            throw new RuntimeException();
+        }
+
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("results.bin"))) {
+            objectOutputStream.writeObject(result);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public Map<LocalDate, Integer> tweetsByDay() {
+        return result.getTweets().stream()
+                .collect(groupingBy(
+                        tweet -> LocalDate.from(tweet.getCreatedAt().toInstant().atZone(ZoneId.of("UTC"))),
+                        summingInt(status -> 1)
+                ));
+    }
+}
---
+++ istddisdead-twitter/src/test/java/name/lemerdy/istdddead/IsTDDDeadFromTwitterTest.java
@@ -0,0 +1,101 @@
+package name.lemerdy.istdddead;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import twitter4j.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.MapEntry.entry;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IsTDDDeadFromTwitterTest {
+
+    @Mock
+    private QueryResult result;
+    
+    private IsTDDDeadFromTwitter isTDDDeadFromTwitter;
+
+    @Before
+    public void createIsTDDDeadFromTwitter() {
+        isTDDDeadFromTwitter = new IsTDDDeadFromTwitter(result);
+    }
+
+    @Test
+    public void should_get_one_tweet() {
+        Status tweet = tweetCreatedAt("1981-12-24T00:00:00.00Z");
+        given(result.getTweets()).willReturn(asList(tweet));
+
+        Map<LocalDate, Integer> tweetsByDay = isTDDDeadFromTwitter.tweetsByDay();
+
+        assertThat(tweetsByDay).containsExactly(entry(LocalDate.of(1981, Month.DECEMBER, 24), 1));
+    }
+
+    @Test
+    public void should_get_two_tweets_at_same_date() {
+        Status firstTweet = tweetCreatedAt("1981-12-24T00:00:00.00Z");
+        Status secondTweet = tweetCreatedAt("1981-12-24T10:00:00.00Z");
+        given(result.getTweets()).willReturn(asList(firstTweet, secondTweet));
+
+        Map<LocalDate, Integer> tweetsByDay = isTDDDeadFromTwitter.tweetsByDay();
+
+        assertThat(tweetsByDay).containsExactly(entry(LocalDate.of(1981, Month.DECEMBER, 24), 2));
+    }
+
+    @Test
+    public void should_get_two_tweets_at_two_dates() {
+        Status firstTweet = tweetCreatedAt("1981-12-24T00:00:00.00Z");
+        Status secondTweet = tweetCreatedAt("2012-09-24T00:00:00.00Z");
+        given(result.getTweets()).willReturn(asList(firstTweet, secondTweet));
+
+        Map<LocalDate, Integer> tweetsByDay = isTDDDeadFromTwitter.tweetsByDay();
+
+        assertThat(tweetsByDay).containsExactly(
+                entry(LocalDate.of(1981, Month.DECEMBER, 24), 1),
+                entry(LocalDate.of(2012, Month.SEPTEMBER, 24), 1)
+        );
+    }
+
+    @Test
+    public void should_get_two_tweets_at_two_dates_order_asc() {
+        Status firstTweet = tweetCreatedAt("2012-09-24T00:00:00.00Z");
+        Status secondTweet = tweetCreatedAt("1981-12-24T00:00:00.00Z");
+        given(result.getTweets()).willReturn(asList(firstTweet, secondTweet));
+
+        Map<LocalDate, Integer> tweetsByDay = isTDDDeadFromTwitter.tweetsByDay();
+
+        assertThat(tweetsByDay).containsExactly(
+                entry(LocalDate.of(1981, Month.DECEMBER, 24), 1),
+                entry(LocalDate.of(2012, Month.SEPTEMBER, 24), 1)
+        );
+    }
+
+    @Test
+    public void should_get_empty_tweets_from_empty_file() throws IOException, ClassNotFoundException {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(getClass().getResourceAsStream("/empty.bin"))) {
+            QueryResult result = (QueryResult) objectInputStream.readObject();
+            Map<LocalDate, Integer> tweetsByDay = new IsTDDDeadFromTwitter(result).tweetsByDay();
+            assertThat(tweetsByDay).isEmpty();
+        }
+    }
+
+    private Status tweetCreatedAt(String date) {
+        Status tweet = mock(Status.class);
+        given(tweet.getCreatedAt()).willReturn(Date.from(Instant.parse(date)));
+        return tweet;
+    }
+}
