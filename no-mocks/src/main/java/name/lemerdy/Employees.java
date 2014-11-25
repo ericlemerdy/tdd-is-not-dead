b38518cbe2dd3bc7b21c297dd5e8828456c79724
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-11-26 00:09:00 +0100

    count by provider

--- no-mocks/src/main/java/name/lemerdy/Employees.java
+++ no-mocks/src/main/java/name/lemerdy/Employees.java
@@ -1,14 +1,19 @@
 package name.lemerdy;
 
+import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.*;
+
 public class Employees {
+    private final List<Employee> employees;
 
     public Employees(Employee... employees) {
+        this.employees = asList(employees);
     }
 
-    public Map<String, Integer> countByEmailProvider() {
-        return null;
+    public Map<String, Long> countByEmailProvider() {
+        return employees.stream().collect(groupingBy(Employee::extractEmailProvider, counting()));
     }
-
 }
